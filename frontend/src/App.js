1ee266251a68283d8a1424f29ad0e21a66063ac4
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2023-04-21 20:01:53 +0530

    This is the final assignment.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import MyForm from "./Assignment/myForm";
 import StudentForm from "./Assignment/studentForm";
 import StudentList from "./Assignment/studentList";
 
 function App() {
   return (
     <div className="App">
-      {/* <MyForm /> */}
-      {/* <StudentForm/> */}
-      <StudentList />
+      <StudentForm/> 
+//       <StudentList />
     </div>
   );
 }
